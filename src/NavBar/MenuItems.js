da0323233d39ca397118d328352349e5f01483e7
Guardian <102593555+SafeGuardian@users.noreply.github.com>
2023-05-16 11:33:26 +0200

    Update MenuItems.js

--- src/NavBar/MenuItems.js
+++ src/NavBar/MenuItems.js
@@ -9,4 +9,19 @@ export const MenuItems = [
     url: "/liquidity",
     cName: "nav-links",
   },
+  {
+    title: "Buy",
+    url: "https://www.mintme.com/token/DogSwap/invite",
+    cName: "nav-links",
+  },
+  {
+    title: "Partner",
+    url: "https://docs.google.com/forms/d/e/1FAIpQLSerAAZ-3Dswnpru2FO7ppxfs1wu1-IR1KsEdXn5N6mreXW6cw/viewform?usp=sf_link",
+    cName: "nav-links",
+  },
+  {
+    title: "Discord",
+    url: "https://discord.com/invite/RSQZDGThfU",
+    cName: "nav-links",
+  },
 ];
