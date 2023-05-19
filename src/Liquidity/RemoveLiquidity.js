8f3861f5c396288b2d1edfff9b1cff6819fb5f08
Gabriël <80226775+DogToken@users.noreply.github.com>
2023-05-19 17:10:18 +0200

    Update RemoveLiquidity.js

--- src/Liquidity/RemoveLiquidity.js
+++ src/Liquidity/RemoveLiquidity.js
@@ -107,7 +107,7 @@ function LiquidityRemover(props) {
   // Turns the account's balance into something nice and readable
   const formatBalance = (balance, symbol) => {
     if (balance && symbol)
-      return parseFloat(balance).toPrecision(8) + " " + symbol;
+      return parseFloat(balance).toPrecision(18) + " " + symbol;
     else return "0.0";
   };
 
