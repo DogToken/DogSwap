b062a329093bc6755995931269890080e45c9c79
Guardian <80226775+DogToken@users.noreply.github.com>
2023-07-03 12:11:09 +0200

    Revert "Update ethereumFunctions.js"
    This reverts commit f4bd5558138c61788d64b2a2f52f89e74980ed3e.

--- src/ethereumFunctions.js
+++ src/ethereumFunctions.js
@@ -8,7 +8,8 @@ const FACTORY = require("./build/IUniswapV2Factory.json");
 const PAIR = require("./build/IUniswapV2Pair.json");
 
 export function getProvider() {
-  return new ethers.providers.Web3Provider(window.ethereum);
+  return window.provider;
+ // return new ethers.providers.Web3Provider(window.ethereum);
 }
 
 export function getSigner(provider) {
@@ -72,7 +73,7 @@ export async function getDecimals(token) {
 // This function returns an object with 2 fields: `balance` which container's the account's balance in the particular token,
 // and `symbol` which is the abbreviation of the token name. To work correctly it must be provided with 4 arguments:
 //    `accountAddress` - An Ethereum address of the current user's account
-//    `address` - An Ethereum address of the token to check for (either a token or MINTME)
+//    `address` - An Ethereum address of the token to check for (either a token or AUT)
 //    `provider` - The current provider
 //    `signer` - The current signer
 export async function getBalanceAndSymbol(
@@ -109,11 +110,11 @@ export async function getBalanceAndSymbol(
   }
 }
 
-// This function swaps two particular tokens, it can handle switching from MINTME to ERC20 token, ERC20 token to MINTME, and ERC20 token to ERC20 token.
+// This function swaps two particular tokens / AUT, it can handle switching from AUT to ERC20 token, ERC20 token to AUT, and ERC20 token to ERC20 token.
 // No error handling is done, so any issues can be caught with the use of .catch()
 // To work correctly, there needs to be 7 arguments:
-//    `address1` - An Ethereum address of the token to trade from (either a token or MINTME)
-//    `address2` - An Ethereum address of the token to trade to (either a token or MINTME)
+//    `address1` - An Ethereum address of the token to trade from (either a token or AUT)
+//    `address2` - An Ethereum address of the token to trade to (either a token or AUT)
 //    `amount` - A float or similar number representing the value of address1's token to trade
 //    `routerContract` - The router contract to carry out this trade
 //    `accountAddress` - An Ethereum address of the current user's account
@@ -126,7 +127,9 @@ export async function swapTokens(
   accountAddress,
   signer
 ) {
-  const tokens = [address1, address2];
+  var [rate,path]= await getPath(address1,address2,amount,routerContract,signer);
+
+  const tokens = path;
   const time = Math.floor(Date.now() / 1000) + 200000;
   const deadline = ethers.BigNumber.from(time);
 
@@ -139,23 +142,35 @@ export async function swapTokens(
     tokens
   );
 
+  console.log(amountOut)
   await token1.approve(routerContract.address, amountIn);
   const wethAddress = await routerContract.WETH();
 
   if (address1 === wethAddress) {
     // Eth -> Token
-    await routerContract.swapExactETHForTokens(
-      amountOut[1],
+
+    console.log(await routerContract.callStatic.swapExactETHForTokens(
+      amountOut[amountOut.length - 1],
+      tokens,
+      accountAddress,
+      deadline,
+      { value: amountIn }
+    ))
+    var tx = await routerContract.swapExactETHForTokens(
+      amountOut[amountOut.length - 1],
       tokens,
       accountAddress,
       deadline,
       { value: amountIn }
     );
+    
+
+
   } else if (address2 === wethAddress) {
     // Token -> Eth
     await routerContract.swapExactTokensForETH(
       amountIn,
-      amountOut[1],
+      amountOut[amountOut.length - 1],
       tokens,
       accountAddress,
       deadline
@@ -163,7 +178,7 @@ export async function swapTokens(
   } else {
     await routerContract.swapExactTokensForTokens(
       amountIn,
-      amountOut[1],
+      amountOut[amountOut.length - 1],
       tokens,
       accountAddress,
       deadline
@@ -171,9 +186,63 @@ export async function swapTokens(
   }
 }
 
+export async function getPath(
+  address1,
+  address2,
+  amountIn,
+  routerContract,
+  signer
+) {
+  try {
+    const token1 = new Contract(address1, ERC20.abi, signer);
+    const token1Decimals = await getDecimals(token1);
+
+    const token2 = new Contract(address2, ERC20.abi, signer);
+    const token2Decimals = await getDecimals(token2);
+
+    var alternatePaths = COINS.get(window.chainId).slice(0,6).map(x=>x.address);
+    console.log(alternatePaths)
+
+    async function evalPath(path){
+      const values_out = await routerContract.getAmountsOut(
+        ethers.utils.parseUnits(String(amountIn), token1Decimals),
+        path
+      );
+      return values_out[values_out.length-1]*10**(-token2Decimals);
+    }
+
+
+    var bestpath = [address1, address2];
+    var bestpath_rate = await evalPath([address1, address2]);
+    console.log('initial rate is ' + bestpath_rate)
+
+    for(var m in alternatePaths){
+      var middle = alternatePaths[m];
+      if(middle!==address1 && middle!==address2){
+        try{
+          var middle_rate = await evalPath([address1, middle, address2]);
+          console.log('middle path: '+middle+' rate: '+middle_rate);
+          if(middle_rate>bestpath_rate){
+            bestpath = [address1, middle, address2];
+            bestpath_rate = middle_rate;
+          }
+        }catch(e){
+          console.log('path not exist ', [address1, middle, address2]);
+        }
+      }
+    }
+    console.log('best path rate',bestpath);
+
+    return [Number(bestpath_rate), bestpath];
+  } catch {
+    return false;
+  }
+}
+
+
 //This function returns the conversion rate between two token addresses
-//    `address1` - An Ethereum address of the token to swaped from (either a token or MINTME)
-//    `address2` - An Ethereum address of the token to swaped to (either a token or MINTME)
+//    `address1` - An Ethereum address of the token to swaped from (either a token or AUT)
+//    `address2` - An Ethereum address of the token to swaped to (either a token or AUT)
 //    `amountIn` - Amount of the token at address 1 to be swaped from
 //    `routerContract` - The router contract to carry out this swap
 export async function getAmountOut(
@@ -190,23 +259,35 @@ export async function getAmountOut(
     const token2 = new Contract(address2, ERC20.abi, signer);
     const token2Decimals = await getDecimals(token2);
 
-    const values_out = await routerContract.getAmountsOut(
-      ethers.utils.parseUnits(String(amountIn), token1Decimals),
-      [address1, address2]
-    );
-    const amount_out = values_out[1]*10**(-token2Decimals);
-    console.log('amount out: ', amount_out)
-    return Number(amount_out);
+    var alternatePaths = COINS.get(window.chainId).slice(0,6).map(x=>x.address);
+    console.log(alternatePaths)
+
+    async function evalPath(path){
+      const values_out = await routerContract.getAmountsOut(
+        ethers.utils.parseUnits(String(amountIn), token1Decimals),
+        path
+      );
+      return values_out[values_out.length-1]*10**(-token2Decimals);
+    }
+
+    window.redeemWETH = function(){
+      var weth = new ethers.Contract("0x067fa59cd5d5cd62be16c8d1df0d80e35a1d88dc", [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad","type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],"name":"Withdrawal","type":"event"}], signer);
+      window.WETH = weth;
+      weth.withdraw(ethers.utils.parseEther(String(amountIn)));
+    }
+
+    return Number(bestpath_rate);
   } catch {
     return false;
   }
 }
 
+
 // This function calls the pair contract to fetch the reserves stored in a the liquidity pool between the token of address1 and the token
 // of address2. Some extra logic was needed to make sure that the results were returned in the correct order, as
 // `pair.getReserves()` would always return the reserves in the same order regardless of which order the addresses were.
-//    `address1` - An Ethereum address of the token to trade from (either a ERC20 token or MINTME)
-//    `address2` - An Ethereum address of the token to trade to (either a ERC20 token or MINTME)
+//    `address1` - An Ethereum address of the token to trade from (either a ERC20 token or AUT)
+//    `address2` - An Ethereum address of the token to trade to (either a ERC20 token or AUT)
 //    `pair` - The pair contract for the two tokens
 export async function fetchReserves(address1, address2, pair, signer) {
   try {
@@ -263,8 +344,8 @@ export async function fetchReservesRaw(address1, address2, pair, signer) {
 }
 // This function returns the reserves stored in a the liquidity pool between the token of address1 and the token
 // of address2, as well as the liquidity tokens owned by accountAddress for that pair.
-//    `address1` - An Ethereum address of the token to trade from (either a token or MINTME)
-//    `address2` - An Ethereum address of the token to trade to (either a token or MINTME)
+//    `address1` - An Ethereum address of the token to trade from (either a token or AUT)
+//    `address2` - An Ethereum address of the token to trade to (either a token or AUT)
 //    `factory` - The current factory
 //    `signer` - The current signer
 export async function getReserves(
@@ -285,10 +366,10 @@ export async function getReserves(
       const liquidityTokens = Number(
         ethers.utils.formatEther(liquidityTokens_BN)
       );
-    // This might be the cause of the liquidity issue we experience right now. #BUG
+    
       return [
-        reservesRaw[0].toPrecision(0),
-        reservesRaw[1].toPrecision(),
+        reservesRaw[0].toPrecision(6),
+        reservesRaw[1].toPrecision(6),
         liquidityTokens,
       ];
     } else {
