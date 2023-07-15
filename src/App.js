b1b6fb5321394208ab2811a50030338893390009
Guardian <80226775+DogToken@users.noreply.github.com>
2023-07-15 16:39:22 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import { createTheme, ThemeProvider } from "@material-ui/core";
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#ff0000",
+      main: "#008e31",
       contrastText: "#ffffff",
     },
     secondary: {
