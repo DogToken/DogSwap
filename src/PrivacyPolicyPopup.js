0a2c8eda0018179e862128af815de9b81a62305e
Gabriël <80226775+DogToken@users.noreply.github.com>
2023-07-25 15:44:30 +0200

    Update PrivacyPolicyPopup.js

--- src/PrivacyPolicyPopup.js
+++ src/PrivacyPolicyPopup.js
@@ -12,6 +12,24 @@ const PrivacyPolicyPopup = () => {
       .then((data) => setPolicyContent(data));
   }, []);
 
+  // Custom styles for the Modal component
+  const customModalStyles = {
+    content: {
+      width: "75%", // Reduce the size to 75% of the original size
+      height: "75%", // Reduce the height to 75% of the original height
+      top: "50%",
+      left: "50%",
+      transform: "translate(-50%, -50%)",
+      overflowY: "auto", // Enable vertical scrolling if content overflows
+    },
+  };
+
+  // Custom styles for the policy content
+  const customPolicyContentStyles = {
+    padding: "20px", // Add padding to make the text readable
+    fontSize: "14px", // Adjust font size for better readability
+  };
+
   return (
     <div>
       <a href="#" onClick={() => setModalIsOpen(true)}>
@@ -21,8 +39,9 @@ const PrivacyPolicyPopup = () => {
         isOpen={modalIsOpen}
         onRequestClose={() => setModalIsOpen(false)}
         contentLabel="Privacy Policy"
+        style={customModalStyles} // Apply custom styles to the Modal component
       >
-        <div dangerouslySetInnerHTML={{ __html: policyContent }} />
+        <div style={customPolicyContentStyles} dangerouslySetInnerHTML={{ __html: policyContent }} />
       </Modal>
     </div>
   );
