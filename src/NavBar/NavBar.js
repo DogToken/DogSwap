a5c9e941335809e493f9d686a785b8251f819e42
Gabriël <80226775+DogToken@users.noreply.github.com>
2023-07-26 00:02:08 +0200

    add sidebar

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -5,27 +5,44 @@ import "./NavBar.css";
 
 const NavBar = () => {
   return (
-    <nav>
-      <div className="Title">
-        <h1 className="navbar-logo">
-          🐶 DogSwap
-        </h1>
-      </div>
-
-      <div className="NavbarItems">
-        <ul className={`nav-menu`}>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <Link className={"nav-links"} to={item.url}>
-                  {item.title}
-                </Link>
-              </li>
-            );
-          })}
+    <div className="container">
+      {/* Sidebar */}
+      <div className="sidebar">
+        {/* Your sidebar content goes here */}
+        <ul>
+          <li>
+            <Link to="/">Sidebar Link 1</Link>
+          </li>
+          <li>
+            <Link to="/">Sidebar Link 2</Link>
+          </li>
+          {/* Add more sidebar links as needed */}
         </ul>
       </div>
-    </nav>
+
+      {/* Navigation Bar */}
+      <nav>
+        <div className="Title">
+          <h1 className="navbar-logo">
+            🐶 DogSwap
+          </h1>
+        </div>
+
+        <div className="NavbarItems">
+          <ul className={`nav-menu`}>
+            {MenuItems.map((item, index) => {
+              return (
+                <li key={index}>
+                  <Link className={"nav-links"} to={item.url}>
+                    {item.title}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
 
