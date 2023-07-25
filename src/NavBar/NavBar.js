29b2bbc83c6dd6b4836d21496e20ee73ad9268dd
Gabriël <80226775+DogToken@users.noreply.github.com>
2023-07-25 02:25:02 +0200

    Update NavBar.js

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -4,9 +4,12 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { MenuItems } from "./MenuItems";
 import "./NavBar.css";
+import { ethers } from "ethers";
+import detectEthereumProvider from '@metamask/detect-provider';
 
 const NavBar = () => {
   const [tvl, setTVL] = useState(null);
+  const [connected, setConnected] = useState(false);
 
   // Function to calculate TVL based on your specific logic (replace with your actual calculation)
   const calculateTVL = (totalSupply, burnedSupply) => {
@@ -40,6 +43,33 @@ const NavBar = () => {
     fetchTVLData();
   }, []);
 
+  useEffect(() => {
+    // Check if MetaMask provider is available
+    detectEthereumProvider().then(provider => {
+      if (provider) {
+        setConnected(true);
+      }
+    });
+  }, []);
+
+  const connectToMetaMask = async () => {
+    try {
+      // Request access to the user's MetaMask accounts
+      const provider = await detectEthereumProvider();
+      if (provider) {
+        await provider.request({ method: 'eth_requestAccounts' });
+        // Once connected, you can interact with the Ethereum blockchain using ethers.js
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        // Perform Ethereum transactions or any other interactions
+        // Example: const signer = provider.getSigner();
+        //          const contract = new ethers.Contract(contractAddress, contractAbi, signer);
+        //          const transaction = await contract.transfer(toAddress, amount);
+      }
+    } catch (error) {
+      console.error('Error connecting to MetaMask:', error);
+    }
+  };
+
   return (
     <nav>
       <div className="Title">
@@ -62,6 +92,15 @@ const NavBar = () => {
             );
           })}
         </ul>
+      </div>
+
+      {/* MetaMask button */}
+      <div className="metamask-button">
+        {connected ? (
+          <button onClick={connectToMetaMask}>Connect to MetaMask</button>
+        ) : (
+          <p>MetaMask not detected.</p>
+        )}
       </div>
     </nav>
   );
