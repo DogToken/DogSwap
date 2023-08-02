3d503445b2eb3a4f84f6cc4b4ea24568d79404b8
Guardian <80226775+DogToken@users.noreply.github.com>
2023-08-02 15:48:42 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,8 +9,14 @@ import { Web3ReactProvider } from "@web3-react/core";
 import { ethers } from "ethers";
 
 function getWeb3Library(provider) {
-  // Return the ethers.js provider here
-  return new ethers.providers.Web3Provider(provider);
+  // Check if the provider is available
+  if (provider && provider.isMetaMask) {
+    // If using MetaMask, return the ethers.js provider wrapping the MetaMask provider
+    return new ethers.providers.Web3Provider(provider);
+  } else {
+    // Otherwise, return the default ethers.js provider (e.g., for testing)
+    return new ethers.providers.JsonRpcProvider("https://node1.mintme.com");
+  }
 }
 
 ReactDOM.render(
