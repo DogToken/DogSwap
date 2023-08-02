23cf03070c87d0f61f8ac1e8f90ca9745ce13a2e
Guardian <80226775+DogToken@users.noreply.github.com>
2023-08-02 13:39:53 +0200

    Update web3.js

--- src/web3.js
+++ src/web3.js
@@ -11,12 +11,12 @@ function getWeb3Provider() {
 
 export const provider = getWeb3Provider();
 
-export async function initializeContracts() {
+export function initializeContracts() {
   const TOKEN_ADDRESS = "0x38924b27e5A43A6D9AD1377eC828C056120f06a0"; // Replace with the actual token address
   const STAKING_ADDRESS = "0x38D613a0636Bd10043405D76e52f7540eeE913d0"; // Replace with the actual staking address
 
   const TOKEN = new ethers.Contract(TOKEN_ADDRESS, TokenABI, provider);
   const STAKING_CONTRACT = new ethers.Contract(STAKING_ADDRESS, StakingAbi, provider);
 
-  return { TOKEN, STAKING_CONTRACT };
+  return Promise.all([TOKEN, STAKING_CONTRACT]);
 }
