83af0da6a17750672d41f987e83f32e5cb3c1c8d
Guardian <80226775+DogToken@users.noreply.github.com>
2023-08-02 15:23:17 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,12 +4,15 @@ import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Web3ReactProvider } from "@web3-react/core";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <BrowserRouter>
     <React.StrictMode>
+    <Web3ReactProvider getLibrary={getLibrary}>
       <App />
+    </Web3ReactProvider>
     </React.StrictMode>
   </BrowserRouter>,
 
