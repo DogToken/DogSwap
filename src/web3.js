8448834bf3c824de3f3c8f5b38bb336387473516
Guardian <80226775+DogToken@users.noreply.github.com>
2023-08-02 14:09:20 +0200

    Update web3.js

--- src/web3.js
+++ src/web3.js
@@ -1,6 +1,6 @@
 import { ethers } from "ethers";
-import TokenABI from "../build/dogToken.abi.json";
-import StakingAbi from "../build/stakingVault.abi.json";
+import TokenABI from "./build/dogToken.abi.json";
+import StakingAbi from "./build/stakingVault.abi.json";
 
 const TOKEN_ADDRESS = "0x38924b27e5A43A6D9AD1377eC828C056120f06a0"; // Replace with the actual token address
 const STAKING_ADDRESS = "0x38D613a0636Bd10043405D76e52f7540eeE913d0"; // Replace with the actual staking address
