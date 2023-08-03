cbda02ef83f96068564f68855f2859b7541d73a3
Gabriël <80226775+DogToken@users.noreply.github.com>
2023-08-04 00:38:25 +0200

    .

--- src/Game/ClickerGame.js
+++ src/Game/ClickerGame.js
@@ -1,54 +1,35 @@
-import React, { useState, useEffect } from 'react';
-import Bank from './Bank';
+import React, { useState } from 'react';
+import { FaDog } from 'react-icons/fa';
 import Shop from './Shop';
+import Bank from './Bank';
 import './styles/game.css';
-import { FaDog, FaCoins, FaEthereum } from 'react-icons/fa';
 
 const ClickerGame = () => {
-  // Initialize state from local storage if available
-  const initialState = {
-    smollTokens: 0,
-    ether: 0,
-    clicks: 0,
-    clickMultiplier: 1,
-    autoClicker: 0,
+  // ... (existing code)
+
+  const [showShop, setShowShop] = useState(false);
+  const [showBank, setShowBank] = useState(false);
+
+  const handleShowShop = () => {
+    setShowShop(true);
   };
 
-  const [state, setState] = useState(() => {
-    const savedState = JSON.parse(localStorage.getItem('dogMinerSave'));
-    return savedState || initialState;
-  });
-
-  // Save state to local storage whenever there's a change
-  useEffect(() => {
-    localStorage.setItem('dogMinerSave', JSON.stringify(state));
-  }, [state]);
-
-  const handleClick = () => {
-    setState((prevState) => ({
-      ...prevState,
-      smollTokens: prevState.smollTokens + prevState.clickMultiplier,
-      clicks: prevState.clicks + 1,
-    }));
+  const handleHideShop = () => {
+    setShowShop(false);
   };
 
-  // Calculate the Ether based on Smoll Tokens
-  useEffect(() => {
-    setState((prevState) => ({
-      ...prevState,
-      ether: Math.floor(prevState.smollTokens / 1337),
-    }));
-  }, [state.smollTokens]);
+  const handleShowBank = () => {
+    setShowBank(true);
+  };
 
-  // Calculate user level based on clicks
-  const calculateUserLevel = () => {
-    return Math.floor(100 * Math.pow(1.1, state.clicks / 10));
+  const handleHideBank = () => {
+    setShowBank(false);
   };
 
   return (
     <div className="container">
-      {/* Stats Box */}
-      <div className="stats-box">
+      {/* Information Box */}
+      <div className="info-box">
         <h2>Stats</h2>
         <div className="stat-item">
           <FaCoins className="currency-icon" /> Smoll Tokens: {state.smollTokens}
@@ -60,7 +41,9 @@ const ClickerGame = () => {
         <div className="stat-item">Awards: (Coming Soon)</div>
         <div className="stat-item">Profile Picture: (Coming Soon)</div>
       </div>
-      <div className="clicker-paper">
+
+      {/* Clicker Box */}
+      <div className="clicker-box">
         <h1>DogMiner - Incremental Clicker Game</h1>
         <div className="center">
           <button className="click-button" onClick={handleClick}>
@@ -68,29 +51,18 @@ const ClickerGame = () => {
           </button>
         </div>
       </div>
-      {/* Side Menu */}
-      <div className="side-menu">
-        <div className="paper">
-          <Shop
-            smollTokens={state.smollTokens}
-            setSmollTokens={(newTokens) => setState((prevState) => ({ ...prevState, smollTokens: newTokens }))}
-            ether={state.ether}
-            setEther={(newEther) => setState((prevState) => ({ ...prevState, ether: newEther }))}
-            clickMultiplier={state.clickMultiplier}
-            setClickMultiplier={(newMultiplier) => setState((prevState) => ({ ...prevState, clickMultiplier: newMultiplier }))}
-            autoClicker={state.autoClicker}
-            setAutoClicker={(newAutoClicker) => setState((prevState) => ({ ...prevState, autoClicker: newAutoClicker }))}
-          />
-        </div>
-        <div className="paper">
-          <Bank
-            smollTokens={state.smollTokens}
-            setSmollTokens={(newTokens) => setState((prevState) => ({ ...prevState, smollTokens: newTokens }))}
-            ether={state.ether}
-            setEther={(newEther) => setState((prevState) => ({ ...prevState, ether: newEther }))}
-          />
-        </div>
+
+      {/* Add buttons to show/hide the pop-ups */}
+      <div className="nav-links">
+        <button onClick={handleShowShop}>Open Shop</button>
+        <button onClick={handleShowBank}>Open Bank</button>
       </div>
+
+      {/* Shop Pop-up */}
+      <Shop open={showShop} handleClose={handleHideShop} />
+
+      {/* Bank Pop-up */}
+      <Bank open={showBank} handleClose={handleHideBank} />
     </div>
   );
 };
