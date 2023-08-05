faffadb8d05471ce5d390ad62df1ccc03d91a8b3
Guardian <80226775+DogToken@users.noreply.github.com>
2023-08-05 23:43:53 +0200

    Update Staking.js

--- src/Staking/Staking.js
+++ src/Staking/Staking.js
@@ -1,159 +1,146 @@
-import React, { useEffect, useState } from "react";
-import {
-  Container,
-  Typography,
-  makeStyles,
-  Paper,
-  TextField,
-  Button,
-  Box,
-} from "@material-ui/core";
-import { ethers } from "ethers";
-import { initializeContracts } from "../web3";
+import React, { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
+import StakingContract from './contracts/StakingContract.json'; // Update the path to your compiled contract JSON
+import { Container, Paper, Typography, Box, TextField, Button, makeStyles } from '@material-ui/core';
+
+const networks = [24734];
+
+export const ChainId = {
+  MINTME: 24734,
+};
+
+export const routerAddress = new Map();
+routerAddress.set(ChainId.MINTME, "0xa6c72da53025c5291d326fadc5a1277aa21fef2c");
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    marginTop: theme.spacing(4),
-    marginBottom: theme.spacing(4),
-    padding: theme.spacing(2),
+    padding: theme.spacing(4),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   title: {
-    marginBottom: theme.spacing(2),
+    marginBottom: theme.spacing(3),
   },
   paragraph: {
     marginBottom: theme.spacing(1),
   },
   formContainer: {
-    display: "flex",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginTop: theme.spacing(3),
   },
   form: {
-    display: "flex",
-    alignItems: "center",
-    marginRight: theme.spacing(2),
-    "& > *": {
-      marginRight: theme.spacing(1),
-    },
+    display: 'flex',
+    alignItems: 'center',
+    marginBottom: theme.spacing(2),
   },
 }));
 
-function Staking({ account }) {
+const StakingDapp = () => {
   const classes = useStyles();
-  const [views, setViews] = useState({});
-  const [stake, setStake] = useState("");
-  const [withdraw, setWithdraw] = useState("");
-  const [isLoaded, setLoaded] = useState(false);
-  const [TOKEN, setToken] = useState(null);
-  const [STAKING_CONTRACT, setStakingContract] = useState(null);
-  const provider = new ethers.providers.JsonRpcProvider("https://node1.mintme.com");
+  const [account, setAccount] = useState('');
+  const [contract, setContract] = useState(null);
+  const [stake, setStake] = useState('');
+  const [withdraw, setWithdraw] = useState('');
+  const [views, setViews] = useState({
+    staked: 0,
+    reward: 0,
+    totalStaked: 0,
+  });
 
+  // Connect to the Ethereum network on component mount
+  useEffect(() => {
+    connectToEthereum();
+  }, []);
+
+  // Function to connect to the Ethereum network and set up the contract
+  const connectToEthereum = async () => {
+    try {
+      if (typeof window.ethereum !== 'undefined') {
+        await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const network = await provider.getNetwork();
+
+        // Check if the connected network is supported
+        if (networks.includes(network.chainId)) {
+          const signer = provider.getSigner();
+          setAccount(await signer.getAddress());
+
+          const contract = new ethers.Contract(routerAddress.get(network.chainId), StakingContract.abi, signer);
+          setContract(contract);
+
+          fetchStakingDetails(); // Fetch the user's staking details
+        } else {
+          console.log('Unsupported network. Please switch to the correct network.');
+        }
+      } else {
+        console.log('Please install MetaMask to use this dApp.');
+      }
+    } catch (error) {
+      console.error('Error connecting to Ethereum:', error);
+    }
+  };
+
+  // Function to fetch the user's staking details and update the views
+  const fetchStakingDetails = async () => {
+    try {
+      const stakingBalance = await contract.stakingBalanceOf(account);
+      const stakingReward = await contract.stakingRewardOf(account);
+      const totalStakedBalance = await contract.totalStakedBalance();
+
+      setViews({
+        staked: ethers.utils.formatUnits(stakingBalance, 18),
+        reward: ethers.utils.formatUnits(stakingReward, 18),
+        totalStaked: ethers.utils.formatUnits(totalStakedBalance, 18),
+      });
+    } catch (error) {
+      console.error('Error fetching staking details:', error);
+    }
+  };
+
+  // Function to handle stake submission
   const handleStake = async (event) => {
     event.preventDefault();
     try {
-      const signer = window.ethereum?.getSigner();
-      if (!signer) {
-        throw new Error("Web3 provider not found. Make sure to connect your wallet.");
-      }
-      const amount = ethers.utils.parseEther(stake);
-
-      const Token = TOKEN.connect(signer);
-      const allowance = await Token.allowance(account, STAKING_CONTRACT.address);
-      if (allowance.lt(amount)) {
-        const tx = await Token.approve(STAKING_CONTRACT.address, amount);
-        await tx.wait();
-      }
-
-      const staking = STAKING_CONTRACT.connect(signer);
-      const tx = await staking.stake(amount);
+      const amount = ethers.utils.parseUnits(stake.toString(), 18);
+      const tx = await contract.stakeTokens(amount);
       await tx.wait();
+      setStake('');
+      fetchStakingDetails();
     } catch (error) {
-      console.error("Error handling stake:", error.message);
+      console.error('Error staking tokens:', error);
     }
   };
 
+  // Function to handle withdraw submission
   const handleWithdraw = async (event) => {
     event.preventDefault();
     try {
-      const signer = window.ethereum?.getSigner();
-      if (!signer) {
-        throw new Error("Web3 provider not found. Make sure to connect your wallet.");
-      }
-      const staking = STAKING_CONTRACT.connect(signer);
-
-      const amount = ethers.utils.parseEther(withdraw);
-      const tx = await staking.withdraw(amount);
+      const amount = ethers.utils.parseUnits(withdraw.toString(), 18);
+      const tx = await contract.unstakeTokens(amount);
       await tx.wait();
+      setWithdraw('');
+      fetchStakingDetails();
     } catch (error) {
-      console.error("Error handling withdraw:", error.message);
+      console.error('Error withdrawing tokens:', error);
     }
   };
 
+  // Function to handle claiming reward
   const handleClaimReward = async () => {
     try {
-      const signer = window.ethereum?.getSigner();
-      if (!signer) {
-        throw new Error("Web3 provider not found. Make sure to connect your wallet.");
-      }
-      const staking = STAKING_CONTRACT.connect(signer);
-
-      const tx = await staking.claimReward();
+      const tx = await contract.claimReward();
       await tx.wait();
+      fetchStakingDetails();
     } catch (error) {
-      console.error("Error handling claim reward:", error.message);
+      console.error('Error claiming reward:', error);
     }
   };
 
-  async function getStakingViews(account) {
-    try {
-      const staking = STAKING_CONTRACT.connect(provider.getSigner());
-      const [staked, reward, totalStaked] = await Promise.all([
-        staking.stakedOf(account),
-        staking.rewardOf(account),
-        staking.totalStaked(),
-      ]);
-      setViews({
-        staked: ethers.utils.formatEther(staked),
-        reward: ethers.utils.formatEther(reward),
-        totalStaked: ethers.utils.formatEther(totalStaked),
-      });
-    } catch (error) {
-      console.error("Error fetching staking views:", error.message);
-    }
-  }
-
-  useEffect(() => {
-    const initialize = async () => {
-      try {
-        const contracts = await initializeContracts(provider);
-        const { TOKEN, STAKING_CONTRACT } = contracts;
-        setToken(TOKEN);
-        setStakingContract(STAKING_CONTRACT);
-        setLoaded(true);
-      } catch (error) {
-        console.error("Error initializing contracts:", error.message);
-      }
-    };
-
-    initialize();
-  }, []);
-
-  useEffect(() => {
-    if (isLoaded && TOKEN && STAKING_CONTRACT && account) {
-      getStakingViews(account);
-    }
-  }, [isLoaded, TOKEN, STAKING_CONTRACT, account]);
-
-  if (!isLoaded) {
-    return (
-      <div>
-        <h2>Staking</h2>
-        <p>Loading...</p>
-      </div>
-    );
-  }
-
   return (
-      <Container>
+    <Container>
       <Paper className={classes.root}>
         <Typography variant="h4" className={classes.title}>
           Staking
@@ -201,6 +188,6 @@ function Staking({ account }) {
       </Paper>
     </Container>
   );
-}
+};
 
-export default Staking;
+export default StakingDapp;
