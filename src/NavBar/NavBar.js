f6305b446420667e77e9e31232e76daf3110b6ae
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-10 03:31:21 +0100

    Update NavBar.js

--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -5,32 +5,6 @@ import './NavBar.css';
 import { ethers } from 'ethers';
 
 const NavBar = () => {
-  const [isConnected, setIsConnected] = useState(false);
-  const [walletAddress, setWalletAddress] = useState('');
-  
-  const connectWithMetaMask = async () => {
-    try {
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner();
-      const address = await signer.getAddress();
-      setIsConnected(true);
-      setWalletAddress(address);
-    } catch (error) {
-      console.error('Error connecting with MetaMask:', error);
-    }
-  };
-
-  const disconnectMetaMask = async () => {
-    try {
-      await window.ethereum.request({ method: 'eth_requestAccounts' });
-      setIsConnected(false);
-      setWalletAddress('');
-    } catch (error) {
-      console.error('Error disconnecting MetaMask:', error);
-    }
-  };
-
   return (
     <nav>
       <div className="Title">
@@ -54,16 +28,6 @@ const NavBar = () => {
             );
           })}
         </ul>
-        {!isConnected && (
-          <button className="connect-button" onClick={connectWithMetaMask}>
-            Connect with MetaMask
-          </button>
-        )}
-        {isConnected && (
-          <div className="wallet-info">
-            <span onClick={disconnectMetaMask} className="disconnect-button">{walletAddress}</span>
-          </div>
-        )}
       </div>
     </nav>
   );
