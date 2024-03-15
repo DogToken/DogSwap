c896d4c82352b0fc9b725a3e8c1b52de485eecb1
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-16 00:34:59 +0100

    Update tvl.js

--- src/tvl/tvl.js
+++ src/tvl/tvl.js
@@ -33,7 +33,10 @@ const useStyles = makeStyles((theme) => ({
 
 const POOLS = [
   { id: 0, name: "$BONE-WMINT", address: "0x21D897515b2C4393F7a23BBa210b271D13CCdF10", reserve0: 0, reserve1: 0 }, // Replace "0x..." with actual address
+  { id: 1, name: "$BONE-USDC", address: "0x0BA7216BD34CAF32d1FBCb9341997328b38a03a3", reserve0: 0, reserve1: 0 }, // Replace "0x..." with actual address
   { id: 2, name: "WMINT-USDC", address: "0x1Ea95048A66455C3852dBE4620A3970831564189", reserve0: 0, reserve1: 0 }, // Replace "0x..." with actual address
+  { id: 3, name: "WMINT-DOGSP", address: "0x07Da7DA47b3C71a023d194ff623ab3a737c46393", reserve0: 0, reserve1: 0 }, // Replace "0x..." with actual address
+  { id: 5, name: "$BONE-DOGSP", address: "0xCfFF901398cB001D740FFf564D2dcc9Dbd898a11", reserve0: 0, reserve1: 0 }, // Replace "0x..." with actual address
   // Add more pools as needed
 ];
 
@@ -101,19 +104,20 @@ const TVLPage = () => {
       // Calculate TVL using the prices obtained
       let tvl = 0;
       for (const pool of POOLS) {
-        // Add your calculation logic here to calculate TVL for each pool based on prices
-        // For example: const poolTVL = reserve0 * priceOfToken0 + reserve1 * priceOfToken1;
-        // Accumulate poolTVL to calculate overall TVL
-        // Example: tvl += poolTVL;
+        const poolReserves = await new Contract(pool.address, pairABI.abi, signer).getReserves();
+        const reserve0 = parseFloat(poolReserves[0]) / Math.pow(10, 18); // Assuming 18 decimals for token0
+        const reserve1 = parseFloat(poolReserves[1]) / Math.pow(10, 6); // Assuming 6 decimals for token1
+        const poolTVL = reserve0 * wmintPriceInUSDC + reserve1 * wmintPriceInUSDC; // Adjust as needed
+        tvl += poolTVL;
       }
   
-      setTVLData(tvl);
+      setTVLData(tvl.toFixed(8)); // Limiting to 8 digits after the comma
       setLoading(false);
     } catch (error) {
       console.error("Error fetching TVL data:", error);
       setLoading(false);
     }
-  };  
+  };    
 
   return (
     <Container className={classes.container}>
@@ -123,7 +127,7 @@ const TVLPage = () => {
       ) : (
         <>
           <Typography variant="h5" className={classes.tvlValue}>
-            {/* Display TVL value here */}
+          <Typography>{tvlData}</Typography>
           </Typography>
           <Box className={classes.space}></Box>
           <Typography variant="subtitle1" className={classes.priceInfo}>
