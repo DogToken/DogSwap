9d5dcb9b0f5a8fc78e02cf4d38e5db4a30438fd2
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-18 23:11:32 +0100

    Injection of speed insights

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ import { createTheme, ThemeProvider } from "@material-ui/core";
 import Footer from "./footer"; // Import the Footer component
 import "./footer.css"; // Import the Footer styles
 import { inject } from '@vercel/analytics';
-import { inject } from "@vercel/speed-insights/react"
 import About from "./About";
 import Contact from "./Contact";
 import Privacy from "./Privacy";
