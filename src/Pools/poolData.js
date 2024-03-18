cbd960369dfd7243c3d24d98f6b8ee331490b006
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-18 19:30:53 +0100

    Update poolData.js

--- src/Pools/poolData.js
+++ src/Pools/poolData.js
@@ -7,5 +7,12 @@ export const poolData = [
       MASTER_CHEF_ADDRESS: '0x4f79af8335d41A98386f09d79D19Ab1552d0b925',
       poolId: 5,
     },
+    {
+        title: '$BONE / MintMe',
+        subTitle: 'Stake your $BONE / MintMe LP tokens and get $BONE rewards!',
+        BONE_TOKEN_ADDRESS: '0x9D8dd79F2d4ba9E1C3820d7659A5F5D2FA1C22eF',
+        MASTER_CHEF_ADDRESS: '0x4f79af8335d41A98386f09d79D19Ab1552d0b925',
+        poolId: 0,
+      },
     // Add more pool data objects here
   ];
