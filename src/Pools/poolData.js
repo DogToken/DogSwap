c32bbd4125913030286d2ae4fc04f0f70f70cad1
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-22 08:46:16 +0100

    Update poolData.js

--- src/Pools/poolData.js
+++ src/Pools/poolData.js
@@ -16,5 +16,13 @@ export const poolData = [
         poolId: 0,
         lpTokenAddress: '0x21D897515b2C4393F7a23BBa210b271D13CCdF10',
       },
+      {
+          title: '$BONE / DogSwap',
+          subTitle: 'Stake your $BONE / DogSwap LP tokens and get $BONE rewards! BONE-US! Get extra $BONE when staking',
+          BONE_TOKEN_ADDRESS: '0x9D8dd79F2d4ba9E1C3820d7659A5F5D2FA1C22eF',
+          MASTER_CHEF_ADDRESS: '0x4f79af8335d41A98386f09d79D19Ab1552d0b925',
+          poolId: 6,
+          lpTokenAddress: '0x07Da7DA47b3C71a023d194ff623ab3a737c46393',
+        },
     // Add more pool data objects here
   ];
