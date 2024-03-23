b37f0d3f2b6e4858c83e2b48e4ddebe980690e4f
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-23 23:35:17 +0100

    Update tvl.js

--- src/tvl/tvl.js
+++ src/tvl/tvl.js
@@ -1,27 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Container, Typography, CircularProgress, Box } from "@material-ui/core";
-
-const useStyles = makeStyles((theme) => ({
-  container: {
-    textAlign: "center",
-    padding: theme.spacing(2),
-    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-    borderRadius: theme.spacing(2),
-    background: "#FFFFFF",
-    maxWidth: 600,
-    margin: "auto",
-  },
-  space: {
-    height: theme.spacing(4),
-  },
-  priceInfo: {
-    marginTop: theme.spacing(2),
-  },
-}));
+import React, { useState, useEffect } from 'react';
 
 const TVLPage = () => {
-  const classes = useStyles();
   const [loading, setLoading] = useState(false);
   const [mintmePrice, setMintmePrice] = useState(null);
 
@@ -32,47 +11,33 @@ const TVLPage = () => {
   const fetchMintmePrice = async () => {
     try {
       setLoading(true);
-  
-      const proxyUrl = 'https://cors-anywhere.herokuapp.com/'; // Replace with your proxy server URL
-      const apiUrl = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=MINTME';
-      const response = await fetch(proxyUrl + apiUrl, {
-        headers: {
-          "X-CMC_PRO_API_KEY": "62467a6a-a3a9-4cc4-9fbf-c2a382627596", // Replace with your API key
-          "Accept": "application/json"
-        }
-      });
-  
-      if (!response.ok) {
+      const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=mintme&vs_currencies=usd');
+      const data = await response.json();
+      const price = data?.mintme?.usd;
+
+      if (price) {
+        setMintmePrice(price);
+      } else {
         throw new Error('Failed to fetch MINTME price');
       }
-  
-      const data = await response.json();
-      const mintmePrice = data.data.MINTME.quote.USD.price;
-      setMintmePrice(mintmePrice);
-  
+
       setLoading(false);
     } catch (error) {
-      console.error("Error fetching MINTME price:", error);
+      console.error('Error fetching MINTME price:', error);
       setLoading(false);
     }
   };
-  
 
   return (
-    <Container className={classes.container}>
-      <Typography variant="h4">MINTME Price</Typography>
+    <div>
       {loading ? (
-        <CircularProgress />
+        <p>Loading...</p>
+      ) : mintmePrice ? (
+        <p>1 MINTME = ${mintmePrice} USD</p>
       ) : (
-        <>
-          <Box className={classes.space}></Box>
-          <Typography variant="subtitle1" className={classes.priceInfo}>
-            1 MINTME = ${mintmePrice} USD
-          </Typography>
-        </>
+        <p>Failed to fetch MINTME price</p>
       )}
-      <Box className={classes.space}></Box>
-    </Container>
+    </div>
   );
 };
 
