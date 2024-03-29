2b0648a2509af36e955b53e61dcb3fe1b760c7c3
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-29 13:29:02 +0100

    fix

--- src/Blog/SinglePost.js
+++ src/Blog/SinglePost.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import posts from './Posts';
 import './SinglePost.css';
 
