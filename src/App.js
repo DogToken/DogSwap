73d4caad8f618c3cf738f142576bb60bd4418e82
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-31 21:02:46 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,27 @@ import React from "react";
 import { ethers } from "ethers";
 import Web3Provider from "./utils/network";
 import NavBar from "./components/NavBar/NavBar";
-import CoinSwapper from "./pages/CoinSwapper/CoinSwapper";
+import CoinSwapper from "./views/CoinSwapper/CoinSwapper";
 import { Route } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
-import Liquidity from "./pages/Liquidity/Liquidity";
+import Liquidity from "./views/Liquidity/Liquidity";
 import { createTheme, ThemeProvider } from "@material-ui/core";
 import Footer from "./components/Footer/footer";
 import "./components/Footer/footer.css";
 import { inject } from '@vercel/analytics';
 import { injectSpeedInsights } from '@vercel/speed-insights';
-import About from "./pages/Info/About";
-import Contact from "./pages/Info/Contact";
-import Privacy from "./pages/Info/Privacy";
-import Staking from "./pages/Staking/Staking";
-import Faucet from "./pages/Faucet/Faucet";
-import TVL from "./pages/tvl/tvl";
-import Pools from "./pages/Pools/pools";
-import Lottery from "./pages/Lottery/Lottery";
-import PostList from './pages/Blog/PostList';
-import SinglePost from './pages/Blog/SinglePost';
+import About from "./views/Info/About";
+import Contact from "./views/Info/Contact";
+import Privacy from "./views/Info/Privacy";
+import Staking from "./views/Staking/Staking";
+import Faucet from "./views/Faucet/Faucet";
+import TVL from "./views/tvl/tvl";
+import Pools from "./views/Pools/pools";
+import Lottery from "./views/Lottery/Lottery";
+import PostList from './views/Blog/PostList';
+import SinglePost from './views/Blog/SinglePost';
 import ReactGA from 'react-ga';
-import Market from './pages/Market/NFTMarketplace'
+import Market from './views/Market/NFTMarketplace'
 
 const TRACKING_ID = "G-PNK1QQHD9M"; // OUR_TRACKING_ID
 ReactGA.initialize(TRACKING_ID);
