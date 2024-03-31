31ed86af021bb7763156fd3083cf1f75751954a6
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-03-31 21:09:47 +0200

    update

--- src/views/tvl/tvl.js
+++ src/views/tvl/tvl.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import { Container, Typography, CircularProgress, Box } from "@material-ui/core";
 import { Contract, ethers } from "ethers";
-import { getProvider, getSigner, getNetwork } from "../ethereumFunctions";
+import { getProvider, getSigner, getNetwork } from "../../utils/ethereumFunctions";
 import pairABI from "../../build/IUniswapV2Pair.json";
 import boneTokenABI from "../../build/bone.json";
 import axios from 'axios';
