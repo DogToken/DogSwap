622b355fb40abd612d464083081bf86f2ef677f1
Gabriël <80226775+DogToken@users.noreply.github.com>
2024-04-13 16:49:43 +0200

    Update poolData.js

--- src/constants/poolData.js
+++ src/constants/poolData.js
@@ -24,5 +24,29 @@ export const poolData = [
           poolId: 6,
           lpTokenAddress: '0x07Da7DA47b3C71a023d194ff623ab3a737c46393',
         },
+        {
+            title: '1000x / MINTME',
+            subTitle: 'Stake your 1000x / MINTM LP tokens and get $BONE rewards!',
+            BONE_TOKEN_ADDRESS: '0x9D8dd79F2d4ba9E1C3820d7659A5F5D2FA1C22eF',
+            MASTER_CHEF_ADDRESS: '0x4f79af8335d41A98386f09d79D19Ab1552d0b925',
+            poolId: 7,
+            lpTokenAddress: '0x34D99393593245F3268ceAcf35a17407C49c4D59',
+          },
+          {
+              title: '1000x / $BONE',
+              subTitle: 'Stake your 1000x / $BONE LP tokens and get $BONE rewards!',
+              BONE_TOKEN_ADDRESS: '0x9D8dd79F2d4ba9E1C3820d7659A5F5D2FA1C22eF',
+              MASTER_CHEF_ADDRESS: '0x4f79af8335d41A98386f09d79D19Ab1552d0b925',
+              poolId: 8,
+              lpTokenAddress: '0x9763E377ce4E6767F7760D9E1FC7E3c2afBc9Cfb',
+            },
+            {
+                title: '1000x / DogSwap',
+                subTitle: 'Stake your 1000x / DogSwap LP tokens and get $BONE rewards!',
+                BONE_TOKEN_ADDRESS: '0x9D8dd79F2d4ba9E1C3820d7659A5F5D2FA1C22eF',
+                MASTER_CHEF_ADDRESS: '0x4f79af8335d41A98386f09d79D19Ab1552d0b925',
+                poolId: 9,
+                lpTokenAddress: '0x0cC0D3382fC2826E18606C968842A91e5C52e2b3',
+              },
     // Add more pool data objects here
   ];
